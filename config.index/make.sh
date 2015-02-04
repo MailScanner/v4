3fb14d2a924ace909a596facdb598c34809e36a7
Jerry.Benton <jerry.benton@mailborder.com>
2015-02-03 23:54:24 -0500

    build.tarball final
    debugged and tested good. (i hope)

--- config.index/make.sh
+++ config.index/make.sh
@@ -7,10 +7,10 @@ fi
 
 # the php files below should be in the same directory
 
-if[ -a create_conf_array.php ]; then
+if [ -a create_conf_array.php ]; then
 	php -q create_conf_array.php > /tmp/conf_array.php
 fi
 
-if[ -a dump_config.php ]; then
+if [ -a dump_config.php ]; then
 	php -q dump_config.php > /tmp/MailScanner.conf.index.html
 fi
