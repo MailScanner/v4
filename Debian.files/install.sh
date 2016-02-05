0941d18c35666661b28142d973a42c859200f294
Jerry.Benton <jerry.benton@mailborder.com>
2016-02-05 04:11:14 -0500

    added MCPAN option
    to perhaps make this thing stop asking questions

--- Debian.files/install.sh
+++ Debian.files/install.sh
@@ -428,6 +428,7 @@ if [ $CPANOPTION == 1 ]; then
 		$CURL -O https://www.mailscanner.info/MyConfig.pm
 		cd $THISCURRPMDIR
 		timewait 1
+		perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
 	fi
 fi
 
